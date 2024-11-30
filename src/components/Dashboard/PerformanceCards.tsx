a23d9b78c72f7c1e77f16a7e7267d3772412f875
nihat cetınkaya <nihatcetinkaya077@gmail.com>
2024-11-30 16:17:45 +0300

    PerformanceCards bileşeni güncellendi: Balance gösterimi eklendi

---
+++ src/components/Dashboard/PerformanceCards.tsx
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+import { Grid, Paper, Typography } from '@mui/material';
+
+interface PerformanceCardsProps {
+  balance: number;
+  isTrading: boolean;
+}
+
+const PerformanceCards: React.FC<PerformanceCardsProps> = ({ balance, isTrading }) => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <Paper elevation={3} sx={{ p: 2 }}>
+          <Typography variant="h6">Balance</Typography>
+          <Typography variant="h4"></Typography>
+        </Paper>
+      </Grid>
+      <Grid item xs={6}>
+        <Paper elevation={3} sx={{ p: 2 }}>
+          <Typography variant="h6">Status</Typography>
+          <Typography variant="h4">{isTrading ? 'Trading' : 'Paused'}</Typography>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default PerformanceCards;
